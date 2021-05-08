48ef82aaa79032da5aea2577987d75af4bd4576e
StrangestAvenger <65728161+StrangestAvenger@users.noreply.github.com>
2021-05-08 18:07:15 +0530

    Delete src directory

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { RegisterRoutes } from './routes/routes';
-
-export const app=express();
-app.set('port',5000);
-
-app.use(express.json());
-RegisterRoutes(app);
